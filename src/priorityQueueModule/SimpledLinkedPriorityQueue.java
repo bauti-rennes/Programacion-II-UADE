198ba1c231c849f3db01d36b948c4830e9a4ac7f
Francisco Fuhr <francisco.fuhr@valtech.com>
2026-04-21 19:54:04 -0300

    Agregar return en funcion simple linked queue priority

--- src/priorityQueueModule/SimpledLinkedPriorityQueue.java
+++ src/priorityQueueModule/SimpledLinkedPriorityQueue.java
@@ -23,6 +23,7 @@ public class SimpledLinkedPriorityQueue<E> implements SimplePriorityQueue<E> {
             first = nodeToAdd;
             last = nodeToAdd;
             size ++;
+            return
         }
 
         //Cuando trabajamos con nodos no podemos usar un for, sino while
