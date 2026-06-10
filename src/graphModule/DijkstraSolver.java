dfc809e75826040e140405300034e77624cc5b77
bauti <bautistarennes88@gmail.com>
2026-06-09 21:02:14 -0300

    clase dijkstra completa

--- src/graphModule/DijkstraSolver.java
+++ src/graphModule/DijkstraSolver.java
@@ -8,18 +8,22 @@ import priorityQueueModule.SimpledLinkedPriorityQueue;
 import setModule.SimpleArraySet;
 import setModule.SimpleSet;
 
-public class DjikstraSolver {
+public class DijkstraSolver {
 
+    //Paso por parámetro un grafo y un origen (un nodo inicial)
+    //Devuelve un diccionario con cada nodo del grafo como key, y un edge con el nodo previo y el peso total hasta ese nodo como value (devuelve la tabla que vimos en clase)
+    // T = Nodo
+    // Edge<T> = (Previo, Costo)
     public static <T> SimpleDictionary<T, Edge<T>> dijkstraAllNodes(Graph<T> graph, T origin) {
 
         //Inicializamos el diccionario ("tabla")
         SimpleDictionary<T, Edge<T>> result = new SimpleArrayDictionary<T, Edge<T>>();
 
-        //Cargamos todos los vertices del grfo al diccionario
+        //Cargamos todos los vertices del grafo al diccionario
         SimpleList<T> vertices = graph.vertices();
         int vertexCount = vertices.size();
 
-        //Inicializamos todos los edges a prev null, weight max
+        //Inicializamos todos los edges a: prev null, weight max (esto es por la teoría que vimos en clase)
         for (int i = 0; i < vertexCount; i++)
         {
             //Inicializo el resultado con todos los vertices y un peso infinito
@@ -35,16 +39,17 @@ public class DjikstraSolver {
         SimplePriorityQueue<T> unvisited = new SimpledLinkedPriorityQueue<T>();
         SimpleSet<T> visited = new SimpleArraySet<T>();
 
-        //Prioridad cero porque es la más alta
+        //Prioridad cero al origen porque es el nodo inicial, y lo agrego a la cola de no visitados
         unvisited.enqueue(origin,0);
 
         //Mientras haya nodos no visitados, visitamos
         while(!unvisited.isEmpty())
         {
-            //Guardamos el nodo actual
+            //Guardamos el nodo actual y lo sacamos de la lista de no visitados
             T current = unvisited.dequeue();
 
             //Si ya lo visitamos, no hay más nada que hacer
+            //En Java, continue hace que se siga con la proxima iteracion del bucle
             if (visited.contains(current)) continue;
 
             //Guardamos el costo total hasta current segun la tabla
@@ -66,17 +71,22 @@ public class DjikstraSolver {
                 //Guardamos el costo total hasta current segun la tabla
                 int totalCost = costToCurrent + neighbors.get(i).weight;
 
+                //Agregamos el vecino a la cola de no visitados
+                unvisited.enqueue(neighbor, totalCost);
+
+                //Si el costo que calculamos es menor al de la tabla
+                //Actualizamos la tabla
 
                 if (totalCost < result.get(neighbor).weight)
                 {
-                    //El nodo previo es el current
+                    //El nodo previo es el current ("destination" es el prev, no confundirse con el siguiente)
                     result.get(neighbor).destination = current;
 
                     //El costo es el que calculamos
                     result.get(neighbor).weight = totalCost;
                 }
             }
-
+            //Lo agrego en la lista de visitados así no lo vuelve a recorrer
             visited.add(current);
         }
 
